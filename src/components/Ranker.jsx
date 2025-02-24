f93e27fedf36d0d0b122f3a2413e376678ccbae0
rohansinha01 <rohansinha01@gmail.com>
2025-02-23 19:58:02 -0500

    data added to page

--- src/components/Ranker.jsx
+++ src/components/Ranker.jsx
@@ -1,8 +1,21 @@
+ import { useEffect, useState } from 'react';
+import data from '../data/movies.json'
 
 const Ranker = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+                setItems(data)
+            }, []);
+ 
   return (
-    <div>Ranker</div>
-  )
-}
+    <div>
+    {items.map((item, index) => (
+                        <div key={index}>
+                            <h3>{item.title}: {item.status}</h3>
+                        </div>
+                    ))}
+                </div>
+            );
+        };
 
 export default Ranker
