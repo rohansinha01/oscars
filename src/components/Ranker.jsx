de35b2e042e81d54fd3472d0b5dcd4088a03b6cd
rohansinha01 <rohansinha01@gmail.com>
2025-02-25 12:06:31 -0500

    useState

--- src/components/Ranker.jsx
+++ src/components/Ranker.jsx
@@ -1,12 +1,14 @@
-
+import { useState } from 'react';
 import data from '../data/movies.json'
 
 const Ranker = () => {
+const [movieVotes, setMovieVotes] = useState('')
   return (
     <div>
     {data.map((item, index) => (
         <div key={index}>
-        <h3>{item.title}: {item.status} <input /></h3>
+        <h3>{item.title}: {item.status} <input type='number' value={movieVotes}
+        onChange={(e) => setMovieVotes(e.target.value)}/></h3>
         </div>
         )
     )
