8d88589dcad947f8051a0baec55a146146b0e04a
rohansinha01 <rohansinha01@gmail.com>
2025-02-25 14:49:03 -0500

    back to basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Ranker from './components/Ranker'
+import data from '../src/data/movies.json'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <Ranker />
+    <Ranker data={data}/>
     </>
   )
 }
--- src/components/Ranker.jsx
+++ src/components/Ranker.jsx
@@ -1,53 +1,18 @@
-import { useState } from 'react';
-import data from '../data/movies.json';
+import data from '../data/movies.json'
 
 const Ranker = () => {
-    const [movieVotes, setMovieVotes] = useState({});
-    const [usedRanks, setUsedRanks] = useState(new Set());
-    const [sortedMovies, setSortedMovies] = useState(data);
+  
+return (
+    <ul>
+        {data.map(({index, title, status}) => (
+            
+           <div key={index}>
+            
+           {title}: {status}
+        </div> 
+        ))}
+    </ul>
+  )
+}
 
-    function handleChange(e, index) {
-        const { value } = e.target;
-        const rank = parseInt(value, 10);
-
-        if (usedRanks.has(rank)) {
-            alert('This rank is already taken. Please choose another rank.');
-            return;
-        }
-
-        setMovieVotes((prevVotes) => {
-            const newVotes = { ...prevVotes, [index]: rank };
-            const newUsedRanks = new Set(Object.values(newVotes));
-            setUsedRanks(newUsedRanks);
-            return newVotes;
-        });
-    }
-
-    function handleClick() {
-        const sorted = [...data].sort((a, b) => {
-            const rankA = movieVotes[data.indexOf(a)] || Infinity;
-            const rankB = movieVotes[data.indexOf(b)] || Infinity;
-            return rankA - rankB;
-        });
-        setSortedMovies(sorted);
-    }
-
-    return (
-        <div>
-            {sortedMovies.map((item, index) => (
-                <div key={index}>
-                    <h3>{item.title}: {item.status}</h3>
-                    <input
-                        type='number'
-                        name='rank'
-                        value={movieVotes[data.indexOf(item)] || ''}
-                        onChange={(e) => handleChange(e, data.indexOf(item))}
-                    />
-                </div>
-            ))}
-            <button onClick={handleClick}>Submit</button>
-        </div>
-    );
-};
-
-export default Ranker;
+export default Ranker
