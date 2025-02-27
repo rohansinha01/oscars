98752a13752eb0dc8d9b89d1e141d9c014479951
rohansinha01 <rohansinha01@gmail.com>
2025-02-26 19:24:27 -0500

    clean code

--- src/components/Ranker.jsx
+++ src/components/Ranker.jsx
@@ -1,18 +1,15 @@
-import data from '../data/movies.json'
+import data from "../data/movies.json";
 
 const Ranker = () => {
-  
-return (
+  return (
     <ul>
-        {data.map(({index, title, status}) => (
-            
-           <div key={index}>
-            
-           {title}: {status}
-        </div> 
-        ))}
+      {data.map(({ index, title, status }) => (
+        <div key={index}>
+          {title}: {status}
+        </div>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Ranker
+export default Ranker;
