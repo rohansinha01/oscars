1a07125a53e579f7e3bdbc6eefb30771e24f6c51
rohansinha01 <rohansinha01@gmail.com>
2025-02-28 00:57:44 -0500

    added #1 option

--- src/components/Ranker.jsx
+++ src/components/Ranker.jsx
@@ -1,19 +1,80 @@
 import data from "../data/movies.json";
-import Button from "./Button";
+import { useState } from "react";
 
-const Ranker = (rank) => {
-  
+const movies = [
+  {
+      "index": 1,
+      "title": "Nomadland",
+      "status": "Winner"
+  },
+  {
+      "index": 2,
+      "title": "The Father",
+      "status": "Nominee"
+  },
+  {
+      "index": 3,
+      "title": "Judas and the Black Messiah",
+      "status": "Nominee"
+  },
+  {
+      "index": 4,
+      "title": "Mank",
+      "status": "Nominee"
+  },
+  {
+      "index": 5,
+      "title": "Minari",
+      "status": "Nominee"
+  },
+  {
+      "index": 6,
+      "title": "Promising Young Woman",
+      "status": "Nominee"
+  },
+  {
+      "index": 7,
+      "title": "Sound of Metal",
+      "status": "Nominee"
+  },
+  {
+      "index": 8,
+      "title": "The Trial of the Chicago 7",
+      "status": "Nominee"
+  }
+]
+
+const Ranker = () => {
+  const [selectedOption, setSelectedOption] = useState('')
   return (
-    <div>
-      <ul>
-        {data.map(({ index, title, status }) => (
-          <div key={index}>
-            {title}: {status} <input />
-          </div>
-        ))}
-      </ul>
-      <Button rank={rank}/>
-    </div>
+
+    <form>
+      <fieldset>
+        <legend>
+          What is your #1 Option?
+        </legend>
+
+        <select
+          value={selectedOption}
+          onChange={event => {
+            setSelectedOption(event.target.value)
+          }}>
+          
+          {movies.map((movie) => (
+            <option key={movie.index} value={movie.title}>
+              {movie.title}
+            </option>
+          ))}
+
+        </select>
+      
+       
+  
+      </fieldset>
+      </form>
+  
+    
+
   );
   
 };
