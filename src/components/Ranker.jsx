e977ea1e6815cd2e683af19184ced14394607df2
rohansinha01 <rohansinha01@gmail.com>
2025-03-01 14:38:24 -0500

    added fourth option

--- src/components/Ranker.jsx
+++ src/components/Ranker.jsx
@@ -6,9 +6,11 @@ const Ranker = () => {
   const [firstOption, setFirstOption] = useState('');
   const [secondOption, setSecondOption] = useState('');
   const [thirdOption, setThirdOption] = useState('');
+  const [fourthOption, setFourthOption] = useState('');
 
   const filteredMovies = movies.filter(movie => movie.title !== firstOption);
   const secondFilteredMovies = movies.filter(movie => movie.title !== firstOption && movie.title !== secondOption);
+  const thirdFilteredMovies = movies.filter(movie => movie.title !== firstOption && movie.title !== secondOption && movie.title !== thirdOption)
  
 
   return (
@@ -71,6 +73,27 @@ const Ranker = () => {
           >
             <option value="" disabled>Select an option</option>
             {secondFilteredMovies.map((movie) => (
+              <option key={movie.index} value={movie.title}>
+                {movie.title}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+      </form>
+      <form>
+        <fieldset>
+          <legend>
+            What is your #4 Option?
+          </legend>
+
+          <select
+            value={fourthOption}
+            onChange={event => {
+              setFourthOption(event.target.value);
+            }}
+          >
+            <option value="" disabled>Select an option</option>
+            {thirdFilteredMovies.map((movie) => (
               <option key={movie.index} value={movie.title}>
                 {movie.title}
               </option>
