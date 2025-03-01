d302f6c46bd93cdb1f12e4cffd496be549003be0
rohansinha01 <rohansinha01@gmail.com>
2025-03-01 15:20:52 -0500

    added more  option

--- src/components/Ranker.jsx
+++ src/components/Ranker.jsx
@@ -1,37 +1,89 @@
-import { useState } from 'react';
-import movies from '../data/movies.json';
-
+import { useState } from "react";
+import movies from "../data/movies.json";
 
 const Ranker = () => {
-  const [firstOption, setFirstOption] = useState('');
-  const [secondOption, setSecondOption] = useState('');
-  const [thirdOption, setThirdOption] = useState('');
-  const [fourthOption, setFourthOption] = useState('');
-  const [fifthOption, setFifthOption] = useState('');
-  const [sixthOption, setSixthOption] = useState('')
+  const [firstOption, setFirstOption] = useState("");
+  const [secondOption, setSecondOption] = useState("");
+  const [thirdOption, setThirdOption] = useState("");
+  const [fourthOption, setFourthOption] = useState("");
+  const [fifthOption, setFifthOption] = useState("");
+  const [sixthOption, setSixthOption] = useState("");
+  const [seventhOption, setSeventhOption] = useState("");
+  const [eighthOption, setEighthOption] = useState("");
+  const [ninthOption, setNinthOption] = useState("");
 
-  const filteredMovies = movies.filter(movie => movie.title !== firstOption);
-  const secondFilteredMovies = movies.filter(movie => movie.title !== firstOption && movie.title !== secondOption);
-  const thirdFilteredMovies = movies.filter(movie => movie.title !== firstOption && movie.title !== secondOption && movie.title !== thirdOption)
-  const fourthFilteredMovies = movies.filter(movie => movie.title !== firstOption && movie.title !== secondOption && movie.title !== thirdOption && movie.title !== fourthOption)
-  const fifthFilteredMovies = movies.filter(movie => movie.title !== firstOption && movie.title !== secondOption && movie.title !== thirdOption && movie.title !== fourthOption && movie.title !== fifthOption)
+  const filteredMovies = movies.filter((movie) => movie.title !== firstOption);
+  const secondFilteredMovies = movies.filter(
+    (movie) => movie.title !== firstOption && movie.title !== secondOption
+  );
+  const thirdFilteredMovies = movies.filter(
+    (movie) =>
+      movie.title !== firstOption &&
+      movie.title !== secondOption &&
+      movie.title !== thirdOption
+  );
+  const fourthFilteredMovies = movies.filter(
+    (movie) =>
+      movie.title !== firstOption &&
+      movie.title !== secondOption &&
+      movie.title !== thirdOption &&
+      movie.title !== fourthOption
+  );
+  const fifthFilteredMovies = movies.filter(
+    (movie) =>
+      movie.title !== firstOption &&
+      movie.title !== secondOption &&
+      movie.title !== thirdOption &&
+      movie.title !== fourthOption &&
+      movie.title !== fifthOption
+  );
+  const sixthFilteredMovies = movies.filter(
+    (movie) =>
+      movie.title !== firstOption &&
+      movie.title !== secondOption &&
+      movie.title !== thirdOption &&
+      movie.title !== fourthOption &&
+      movie.title !== fifthOption &&
+      movie.title !== sixthOption
+  );
+  const seventhFilteredMovies = movies.filter(
+    (movie) =>
+      movie.title !== firstOption &&
+      movie.title !== secondOption &&
+      movie.title !== thirdOption &&
+      movie.title !== fourthOption &&
+      movie.title !== fifthOption &&
+      movie.title !== sixthOption &&
+      movie.title !== seventhOption
+  );
 
+  const eighthFilteredMovies = movies.filter(
+    (movie) =>
+      movie.title !== firstOption &&
+      movie.title !== secondOption &&
+      movie.title !== thirdOption &&
+      movie.title !== fourthOption &&
+      movie.title !== fifthOption &&
+      movie.title !== sixthOption &&
+      movie.title !== seventhOption &&
+      movie.title !== eighthOption
+  );
 
   return (
     <>
       <form>
         <fieldset>
-          <legend>
-            What is your #1 Option?
-          </legend>
+          <legend>What is your #1 Option?</legend>
 
           <select
             value={firstOption}
-            onChange={event => {
+            onChange={(event) => {
               setFirstOption(event.target.value);
             }}
           >
-            <option value="" disabled>Select an option</option>
+            <option value="" disabled>
+              Select an option
+            </option>
             {movies.map((movie) => (
               <option key={movie.index} value={movie.title}>
                 {movie.title}
@@ -40,20 +92,20 @@ const Ranker = () => {
           </select>
         </fieldset>
       </form>
-      
+
       <form>
         <fieldset>
-          <legend>
-            What is your #2 Option?
-          </legend>
+          <legend>What is your #2 Option?</legend>
 
           <select
             value={secondOption}
-            onChange={event => {
+            onChange={(event) => {
               setSecondOption(event.target.value);
             }}
           >
-            <option value="" disabled>Select an option</option>
+            <option value="" disabled>
+              Select an option
+            </option>
             {filteredMovies.map((movie) => (
               <option key={movie.index} value={movie.title}>
                 {movie.title}
@@ -65,17 +117,17 @@ const Ranker = () => {
 
       <form>
         <fieldset>
-          <legend>
-            What is your #3 Option?
-          </legend>
+          <legend>What is your #3 Option?</legend>
 
           <select
             value={thirdOption}
-            onChange={event => {
+            onChange={(event) => {
               setThirdOption(event.target.value);
             }}
           >
-            <option value="" disabled>Select an option</option>
+            <option value="" disabled>
+              Select an option
+            </option>
             {secondFilteredMovies.map((movie) => (
               <option key={movie.index} value={movie.title}>
                 {movie.title}
@@ -87,17 +139,17 @@ const Ranker = () => {
 
       <form>
         <fieldset>
-          <legend>
-            What is your #4 Option?
-          </legend>
+          <legend>What is your #4 Option?</legend>
 
           <select
             value={fourthOption}
-            onChange={event => {
+            onChange={(event) => {
               setFourthOption(event.target.value);
             }}
           >
-            <option value="" disabled>Select an option</option>
+            <option value="" disabled>
+              Select an option
+            </option>
             {thirdFilteredMovies.map((movie) => (
               <option key={movie.index} value={movie.title}>
                 {movie.title}
@@ -109,17 +161,17 @@ const Ranker = () => {
 
       <form>
         <fieldset>
-          <legend>
-            What is your #5 Option?
-          </legend>
+          <legend>What is your #5 Option?</legend>
 
           <select
             value={fifthOption}
-            onChange={event => {
+            onChange={(event) => {
               setFifthOption(event.target.value);
             }}
           >
-            <option value="" disabled>Select an option</option>
+            <option value="" disabled>
+              Select an option
+            </option>
             {fourthFilteredMovies.map((movie) => (
               <option key={movie.index} value={movie.title}>
                 {movie.title}
@@ -131,17 +183,17 @@ const Ranker = () => {
 
       <form>
         <fieldset>
-          <legend>
-            What is your #6 Option?
-          </legend>
+          <legend>What is your #6 Option?</legend>
 
           <select
             value={sixthOption}
-            onChange={event => {
+            onChange={(event) => {
               setSixthOption(event.target.value);
             }}
           >
-            <option value="" disabled>Select an option</option>
+            <option value="" disabled>
+              Select an option
+            </option>
             {fifthFilteredMovies.map((movie) => (
               <option key={movie.index} value={movie.title}>
                 {movie.title}
@@ -149,7 +201,73 @@ const Ranker = () => {
             ))}
           </select>
         </fieldset>
-      </form>      
+      </form>
+
+      <form>
+        <fieldset>
+          <legend>What is your #7 Option?</legend>
+
+          <select
+            value={seventhOption}
+            onChange={(event) => {
+              setSeventhOption(event.target.value);
+            }}
+          >
+            <option value="" disabled>
+              Select an option
+            </option>
+            {sixthFilteredMovies.map((movie) => (
+              <option key={movie.index} value={movie.title}>
+                {movie.title}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+      </form>
+
+      <form>
+        <fieldset>
+          <legend>What is your #8 Option?</legend>
+
+          <select
+            value={eighthOption}
+            onChange={(event) => {
+              setEighthOption(event.target.value);
+            }}
+          >
+            <option value="" disabled>
+              Select an option
+            </option>
+            {seventhFilteredMovies.map((movie) => (
+              <option key={movie.index} value={movie.title}>
+                {movie.title}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+      </form>
+
+      <form>
+        <fieldset>
+          <legend>What is your #9 Option?</legend>
+
+          <select
+            value={ninthOption}
+            onChange={(event) => {
+              setNinthOption(event.target.value);
+            }}
+          >
+            <option value="" disabled>
+              Select an option
+            </option>
+            {eighthFilteredMovies.map((movie) => (
+              <option key={movie.index} value={movie.title}>
+                {movie.title}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+      </form>
     </>
   );
 };
